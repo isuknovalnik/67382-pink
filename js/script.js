47d85687297e9e38add1bdcde89e4281c99cfaec
Inna Suknovalnik <inna.pushistaya@gmail.com>
2015-10-17 13:47:56 +0600

    Исправления мобильной версии

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ link.addEventListener("click", function(event) {
   menu.classList.toggle("main-nav__list--show");
   header.classList.toggle("main-header--transparent");
   if (menu.classList.contains("main-nav__list--show")) {
-    link.innerHTML = '<img src="img/btn-close-menu.png" height="22" width="23" alt="close menu">';
+    link.innerHTML = '<img src="img/menu-cross.svg" height="22" width="23" alt="close menu">';
   }
   else {
-    link.innerHTML = '<img src="img/btn-open-menu.png" height="24" width="50" alt="open menu">';
+    link.innerHTML = '<img src="img/menu-burger.svg" height="24" width="50" alt="open menu">';
   }
 
 });
@@ -20,7 +20,7 @@ window.addEventListener("keydown", function(event) {
     if (menu.classList.contains("main-nav__list--show")) {
       menu.classList.remove("main-nav__list--show");
       header.classList.add("main-header--transparent");
-    link.innerHTML = '<img src="img/btn-open-menu.png" height="24" width="50" alt="open menu">';
+    link.innerHTML = '<img src="img/menu-burger.svg" height="24" width="50" alt="open menu">';
     }
   }
 });
